ed1f94dbf2c6064e3852ded601bca2de9e3aeef5
korollzoilist <g4763862@gmail.com>
2021-01-10 18:10:01 +0500

    A virgin coder skill

---
+++ shit.py
@@ -0,0 +1,9 @@
+shit1 = ['H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd']
+shit2 = None
+
+def fuck_you():
+	for i in range(69420):
+		print('Fuck you')
+
+if __name__ == '__main__':
+	fuck_you()
---
+++ virgin_code.py
@@ -0,0 +1,16 @@
+from random import choices as suck_my_dick
+import shit
+
+your_gf = shit.shit1
+your_dreams = shit.shit2
+
+def life_of_sam():
+	while your_dreams != your_gf:
+		your_dreams = choices(your_gf, k=len(your_gf))
+		print(your_dreams)
+
+small_pp = input('Who are you?')
+if small_pp.lowercase() != 'smallPpPerson':
+	raise ModuleNotFoundError('Your pp is too big for this')
+else:
+	life_of_sam()
