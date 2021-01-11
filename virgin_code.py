f478def2415c119d102c1ceb6ee2a707839b158e
korollzoilist <g4763862@gmail.com>
2021-01-11 15:50:01 +0500

    Even PHP is better than you

--- virgin_code.py
+++ virgin_code.py
@@ -1,15 +1,15 @@
 from random import choices as suck_my_dick
 import shit
 
-your_gf = shit.shit1
-your_dreams = shit.shit2
+your_wet_dreams = shit.shit1
+reality = shit.shit2
 
 def life_of_sam():
-	global your_dreams
-	global your_gf
-	while your_dreams != your_gf:
-		your_dreams = suck_my_dick(your_gf, k=len(your_gf))
-		print(''.join(your_dreams))
+	global your_wet_dreams
+	global reality
+	while your_wet_dreams != reality:
+		reality = suck_my_dick(your_wet_dreams, k=len(your_wet_dreams))
+		print(''.join(reality))
 
 small_pp = input('Who are you? ')
 if small_pp != 'smallPpPerson':
